e700b6f74aeb8c77545cb4b3ab5b264abda0ea95
Shisha1536 <schischkoff2017@yandex.ru>
2023-12-01 15:58:35 +0300

    js++

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ let blockComment = document.getElementsByClassName('block-comment');
 const btnSend = document.getElementById('btn-send');
 let textComment;
 
-debugger
 btnSend.addEventListener('click', () => {
+    debugger
+    let stDate = new Date();
+    console.log(stDate.getDate());
+    console.log(stDate.getMonth()+1);
+    console.log(stDate.getHours());
+    console.log(stDate.getMinutes());
     textComment = document.getElementsByTagName('input');
+    let comment = document.createElement('div');
+    let cat = document.createElement('img');
+    let mainBox = document.createElement('div');
+    let blockNicknameDate = document.createElement('div');
+    let name = document.createElement('p');
+    let data = document.createElement('p');
+    let textCommentBlock = document.createElement('div');
     
 });
