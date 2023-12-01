4dbffb534d4a1b25d65c56b19df76c7080a4b0f2
Shisha1536 <schischkoff2017@yandex.ru>
2023-12-01 12:08:31 +0300

    js+

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-"use strict";
+let numberСomments = document.getElementsByClassName('number-comments');
+let numberFavorites = document.getElementsByClassName('number-favorites');
+let blockComment = document.getElementsByClassName('block-comment');
+const btnSend = document.getElementById('btn-send');
+let textComment;
+
+debugger
+btnSend.addEventListener('click', () => {
+    textComment = document.getElementsByTagName('input');
+    
+});
